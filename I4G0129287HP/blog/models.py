1174bcf940ef5558444a67e131c6ab9c41fb36c7
Datamaven1 <101432050+DataMaven1@users.noreply.github.com>
2022-06-19 12:24:51 +0100

    django_models

--- I4G0129287HP/blog/models.py
+++ I4G0129287HP/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import get_user_model
 
-# Create your models here.
+# Create your models here
+
+User=get_user_model()
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
